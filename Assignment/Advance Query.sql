afdd9541dc69c90a082e601ffaf475e6c3d84c07
unknown <sohailshaikh2379@gmail.com>
2023-10-05 15:58:05 +0530

    Update Advance Query

--- Assignment/Advance Query.sql
+++ Assignment/Advance Query.sql
@@ -15,7 +15,21 @@ GROUP BY c.customer_id, c.first_name, c.last_name
 HAVING COUNT(DISTINCT p.category_id) >= 3;
 
 -- 11. Find the products with the highest and lowest average customer ratings (if a rating table is available).
--- N/A
+-- To find the products with the highest and lowest average customer ratings, assuming we have a Ratings table with product_id and rating columns, we could use the following SQL query:
+WITH AvgRatings AS (
+    SELECT product_id, AVG(rating) AS avg_rating
+    FROM Ratings
+    GROUP BY product_id
+),
+MaxMinRating AS (
+    SELECT MAX(avg_rating) AS max_rating, MIN(avg_rating) AS min_rating FROM AvgRatings
+)
+SELECT p.product_id, p.product_name, ar.avg_rating
+FROM Products p
+JOIN AvgRatings ar ON p.product_id = ar.product_id
+JOIN MaxMinRating mmr ON ar.avg_rating = mmr.max_rating OR ar.avg_rating = mmr.min_rating;
+-- Not able to test as rating table not available
+
 
 -- 12. Calculate the total revenue generated from each category.
 SELECT c.category_id, c.category_name, SUM(o.total_amount) as total_revenue
